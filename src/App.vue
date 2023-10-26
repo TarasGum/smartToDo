7cefb38415df004a33251e03b36f7a4674f2c0e2
TarasGum <tarasgumqqq@gmail.com>
2023-10-26 16:13:42 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<style src="./style.css">
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
